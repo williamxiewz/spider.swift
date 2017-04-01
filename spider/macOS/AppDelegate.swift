7fa2474184ee2e238f6e403a6f8c75d776e4fa37
williamxie <hyw540667209@gmail.com>
2017-04-01 16:35:04 +0800

    初始化
    初始化项目

---
+++ spider/macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS
+//
+//  Created by williamxie on 01/04/2017.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ spider/macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS
+//
+//  Created by williamxie on 01/04/2017.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
